0ed793c922c7420663084a97b5308e949e6fbfcc
renildomarcio <2183646+psycodeliccircus@users.noreply.github.com>
2023-04-09 03:28:34 -0300

    Update 1.0.0

--- php/api.php
+++ php/api.php
@@ -2,7 +2,7 @@
 ini_set("display_errors", 0);
 
 // Set up API key, URL, and model
-$API_KEY = 'sua chave aqui!';
+$API_KEY = 'sua chave aqui.';
 $url = 'https://api.openai.com/v1/completions';
 $model = "text-davinci-003";
 $chat = "";
